91f1271db89ef5952c3c023d254875737450254d
Jack Kill <jackkillschool@gmail.com>
2023-01-10 16:59:34 -0600

    Hello, World!
    Got Electron established at a HW level

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+let win = null;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 300,
+        height: 800,
+        resizable: false,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html');
+};
+
+app.whenReady().then(createWindow);
